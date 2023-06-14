4801b7d56e4f648b8a662cf5f6a4c66e6c9d2171
HP <HP@LAPTOP-54M7IE7C>
2023-06-14 12:10:30 +0530

    updated first commit

--- Maven_project/src/main/java/sample/Sample.java
+++ Maven_project/src/main/java/sample/Sample.java
@@ -5,7 +5,8 @@ public class Sample {
 	public static void main(String[] args) 
 	{
 		int a = 6;
-		System.out.println(a);// TODO Auto-generated method stub
+		System.out.println(a);
+		System.out.println("hi");// TODO Auto-generated method stub
 
 	}
 
