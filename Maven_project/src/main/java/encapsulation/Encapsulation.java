a6fdbc83543418fc4941ffe9db10629b6597fc77
HP <HP@LAPTOP-54M7IE7C>
2023-06-15 10:56:44 +0530

    Encapsulation

---
+++ Maven_project/src/main/java/encapsulation/Encapsulation.java
@@ -0,0 +1,25 @@
+package encapsulation;
+
+public class Encapsulation 
+{
+	private String a;
+	private int b;
+	public void getName()
+	{
+		System.out.println(a);
+		System.out.println(b);
+	}
+	public void setName(String a,int b)
+	{
+		this.a = a;
+		this.b = b;
+	}
+	
+}
+	
+		
+
+
+	
+
+
---
+++ Maven_project/src/main/java/encapsulation/Encapsulation1.java
@@ -0,0 +1,14 @@
+package encapsulation;
+
+public class Encapsulation1
+{
+
+	public static void main(String[] args)
+	{
+		Encapsulation obj = new Encapsulation();
+		obj.setName("Sanju", 25);
+		obj.getName();
+
+	}
+
+}
